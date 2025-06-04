7d91148b3808b470cbfc4aa56dc83c023f578ce8
Xacled <carlox7v7@gmail.com>
2025-06-04 00:01:31 -0300

    Deploy con azure static

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Repo_IC_CD', // Esto es para que funcione en GitHub Pages
   test: {
     globals: true, // para usar describe, it, expect sin importar
     environment: 'jsdom', // Me es necesario para poder testear componentes web
-    setupFiles: './setupTest.js' //Este es para la configuraciones globales de vitest
+    setupFiles: './setupTest.js' //Si tuviesde configuraciones globales
   }
 });
