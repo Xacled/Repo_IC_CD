a6e0295e818e0fce0d63d4cf79c9526b3fd4e831
Xacled <carlox7v7@gmail.com>
2025-06-03 23:50:31 -0300

    Deploy gh pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Repo_IC_CD/', // Esto es para que funcione en GitHub Pages
+  base: '/Repo_IC_CD', // Esto es para que funcione en GitHub Pages
   test: {
     globals: true, // para usar describe, it, expect sin importar
     environment: 'jsdom', // Me es necesario para poder testear componentes web
